a4e09036afdc3220c5835f1d4da455df48b04dc2
TIEB62 -- Titouan Petit <39233377+petittitouan@users.noreply.github.com>
2020-04-16 20:02:07 +0200

    Update ServerType.vb

--- ServerType.vb
+++ ServerType.vb
@@ -1,14 +1,23 @@
 ﻿Public Class ServerType
-    Public ServerType As String
-    'Définition des constantes de type de serveur
-    Public ReadOnly VANILLA As String = "Vanilla"
-    Public ReadOnly SPIGOT As String = "Spigot"
-    Public ReadOnly FORGE As String = "Forge"
-    Public ReadOnly SPONGE As String = "Sponge"
+
 
     Private Sub SpongeRadio_CheckedChanged(sender As Object, e As EventArgs) Handles SpongeRadio.CheckedChanged
         If SpongeRadio.Checked Then
             MsgBox("Attention !" + vbNewLine + "Sponge est un MOD, cela aura donc pour effet de créer un serveur Forge." + vbNewLine + "Seuls les plugins fournis par Sponge pourront être installés !", vbOKOnly + vbExclamation + 4096, "Avertissement")
         End If
+    End Sub
+
+    Private Sub Suivant_Click(sender As Object, e As EventArgs) Handles Suivant.Click
+        If VanillaRadio.Checked Then
+            Informations.ServerType = ServerTypes.VANILLA
+        ElseIf SpigotRadio.Checked Then
+            Informations.ServerType = ServerTypes.SPIGOT
+        ElseIf ForgeRadio.Checked Then
+            Informations.ServerType = ServerTypes.FORGE
+        ElseIf SpongeRadio.Checked Then
+            Informations.ServerType = ServerTypes.SPONGE
+        End If
+        Me.Close()
+        SelectServerDir.Show()
     End Sub
 End Class
