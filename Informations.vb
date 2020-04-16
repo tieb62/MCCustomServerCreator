d5d7ac89f7d869ed861c62363e56edb2f642a462
TIEB62 -- Titouan Petit <39233377+petittitouan@users.noreply.github.com>
2020-04-16 20:00:51 +0200

    Create Informations.vb for storing Server Informations

---
+++ Informations.vb
@@ -0,0 +1,14 @@
+﻿Imports System.IO
+
+Public Module Informations
+    Public ServerType As String
+    'Définition des constantes de type de serveur
+    Public Enum ServerTypes
+        VANILLA
+        SPIGOT
+        FORGE
+        SPONGE
+    End Enum
+
+    Public ServerDir As String
+End Module
