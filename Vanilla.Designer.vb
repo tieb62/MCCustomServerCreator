fa0dc0f415039942149fd4e1027bb9d979be282a
TIEB62 -- Titouan Petit <petit.titouan1@gmail.com>
2020-04-17 15:16:09 +0200

    Updated Vanilla Window

--- Vanilla.Designer.vb
+++ Vanilla.Designer.vb
@@ -23,13 +23,14 @@ Partial Class Vanilla
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.VersionsList = New System.Windows.Forms.ListView()
         Me.VersionType = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.VersionName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.SuspendLayout()
         '
         'Button1
         '
+        Me.Button1.Enabled = False
         Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 25.0!)
         Me.Button1.Location = New System.Drawing.Point(12, 374)
         Me.Button1.Name = "Button1"
@@ -38,30 +39,34 @@ Partial Class Vanilla
         Me.Button1.Text = "Suivant"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'ListView1
+        'VersionsList
         '
-        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.VersionType, Me.VersionName})
-        Me.ListView1.HideSelection = False
-        Me.ListView1.Location = New System.Drawing.Point(12, 12)
-        Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(275, 356)
-        Me.ListView1.TabIndex = 2
-        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.VersionsList.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.VersionName, Me.VersionType})
+        Me.VersionsList.HideSelection = False
+        Me.VersionsList.Location = New System.Drawing.Point(12, 12)
+        Me.VersionsList.MultiSelect = False
+        Me.VersionsList.Name = "VersionsList"
+        Me.VersionsList.Size = New System.Drawing.Size(275, 356)
+        Me.VersionsList.TabIndex = 2
+        Me.VersionsList.UseCompatibleStateImageBehavior = False
+        Me.VersionsList.View = System.Windows.Forms.View.Details
         '
         'VersionType
         '
         Me.VersionType.Text = "Type"
+        Me.VersionType.Width = 121
         '
         'VersionName
         '
         Me.VersionName.Text = "Nom"
+        Me.VersionName.Width = 144
         '
         'Vanilla
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(299, 450)
-        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.VersionsList)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Vanilla"
         Me.Text = "Vanilla"
@@ -70,7 +75,7 @@ Partial Class Vanilla
     End Sub
 
     Friend WithEvents Button1 As Button
-    Friend WithEvents ListView1 As ListView
+    Friend WithEvents VersionsList As ListView
     Friend WithEvents VersionType As ColumnHeader
     Friend WithEvents VersionName As ColumnHeader
 End Class
--- Vanilla.vb
+++ Vanilla.vb
@@ -1,5 +1,65 @@
-﻿Public Class Vanilla
-    Private Sub Vanilla_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
+Imports Newtonsoft.Json.Linq
 
+Public Class Vanilla
+    'Création du Dictionnaire contenant la liste des versions et leurs liens
+    Public Versions As New Dictionary(Of String, String)
+
+    Private Sub Vanilla_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UpdateVersionList()
+    End Sub
+
+    Private Sub UpdateVersionList()
+        If CheckInternet() Then
+            If File.Exists(Informations.ManifestDownloadPath) Then
+                File.Delete(Informations.ManifestDownloadPath)
+            End If
+            Try
+                My.Computer.Network.DownloadFile(Informations.ManifestURL, Informations.ManifestDownloadPath)
+#Disable Warning CA1031 ' Do not catch general exception types
+            Catch ex As Exception
+                MsgBox("Une erreur est survenue lors de l'obtention de la liste des versions :" + vbNewLine + ex.Message, vbOKOnly + vbCritical + 4096, "Erreur")
+#Enable Warning CA1031 ' Do not catch general exception types
+            End Try
+            Dim VersionManifestJSON As String = File.ReadAllText(Informations.ManifestDownloadPath)
+
+
+            Dim obj As JObject = JObject.Parse(VersionManifestJSON)
+
+            VersionsList.BeginUpdate()
+
+            Dim VersionsJSON = obj("versions").ToList()
+            For Each VersionJSON As JToken In VersionsJSON
+                Dim VersionObj As JObject = JObject.Parse(VersionJSON.ToString())
+                Dim versionInfo As New ListViewItem
+                Dim VersionName As String
+                Dim VersionLink As String
+                For Each version As JProperty In VersionObj.Children()
+                    version.CreateReader()
+                    Select Case version.Name
+                        Case "id"
+                            versionInfo.Text = version.Value
+                            VersionName = version.Value
+                        Case "type"
+                            versionInfo.SubItems.Add(version.Value)
+                            VersionLink = version.Value
+                    End Select
+                Next
+                VersionsList.Items.Add(versionInfo)
+#Disable Warning BC42104 ' La variable est utilisée avant de se voir attribuer une valeur
+                Versions.Add(VersionName, VersionLink)
+#Enable Warning BC42104 ' La variable est utilisée avant de se voir attribuer une valeur
+            Next
+            VersionsList.EndUpdate()
+            VersionsList.Update()
+        Else
+            MsgBox("Pas d'accès internet !", vbOKOnly + vbCritical + 4096, "Erreur")
+        End If
+    End Sub
+
+    Private Sub VersionsList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles VersionsList.SelectedIndexChanged
+        If VersionsList.SelectedItems.Count = 1 Then
+            Downloader.DownloadVanillaServer(VersionsList.SelectedItems.Item(0).Name, Me, Eula)
+        End If
     End Sub
 End Class
