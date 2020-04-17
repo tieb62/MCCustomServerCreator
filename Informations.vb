cdbb84b6eacd272aa01810e698f5dfb726f03afe
TIEB62 -- Titouan Petit <petit.titouan1@gmail.com>
2020-04-17 15:29:47 +0200

    Update Informations.vb

--- Informations.vb
+++ Informations.vb
@@ -11,4 +11,11 @@ Public Module Informations
     End Enum
 
     Public ServerDir As String
+
+    'URLs
+    Public ReadOnly ManifestURL As String = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+
+    'Chemins d'accès
+    Public ReadOnly TempPath As String = Directory.CreateDirectory(Path.GetTempPath() + "\MCCustomServerCreator").FullName
+    Public ReadOnly ManifestDownloadPath As String = TempPath + "\version_manifest.json"
 End Module
