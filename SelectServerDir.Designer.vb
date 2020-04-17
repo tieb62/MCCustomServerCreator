a7b52e4d1430616627e0a07a70c02f0d06c84cdf
TIEB62 -- Titouan Petit <39233377+petittitouan@users.noreply.github.com>
2020-04-17 15:04:57 +0200

    Updated SelectServerDir Window

--- SelectServerDir.Designer.vb
+++ SelectServerDir.Designer.vb
@@ -26,20 +26,23 @@ Partial Class SelectServerDir
         Me.Browse = New System.Windows.Forms.Button()
         Me.NextBtn = New System.Windows.Forms.Button()
         Me.SelectDir = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ServerDirLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ServerPath
         '
-        Me.ServerPath.Location = New System.Drawing.Point(12, 12)
+        Me.ServerPath.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ServerPath.Location = New System.Drawing.Point(12, 49)
         Me.ServerPath.Name = "ServerPath"
-        Me.ServerPath.Size = New System.Drawing.Size(647, 20)
+        Me.ServerPath.Size = New System.Drawing.Size(626, 26)
         Me.ServerPath.TabIndex = 0
         '
         'Browse
         '
-        Me.Browse.Location = New System.Drawing.Point(665, 12)
+        Me.Browse.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Browse.Location = New System.Drawing.Point(644, 49)
         Me.Browse.Name = "Browse"
-        Me.Browse.Size = New System.Drawing.Size(28, 20)
+        Me.Browse.Size = New System.Drawing.Size(32, 26)
         Me.Browse.TabIndex = 1
         Me.Browse.Text = "..."
         Me.Browse.UseVisualStyleBackColor = True
@@ -47,9 +50,9 @@ Partial Class SelectServerDir
         'NextBtn
         '
         Me.NextBtn.Font = New System.Drawing.Font("Comic Sans MS", 20.0!)
-        Me.NextBtn.Location = New System.Drawing.Point(12, 38)
+        Me.NextBtn.Location = New System.Drawing.Point(12, 81)
         Me.NextBtn.Name = "NextBtn"
-        Me.NextBtn.Size = New System.Drawing.Size(681, 47)
+        Me.NextBtn.Size = New System.Drawing.Size(664, 47)
         Me.NextBtn.TabIndex = 2
         Me.NextBtn.Text = "Suivant"
         Me.NextBtn.UseVisualStyleBackColor = True
@@ -59,11 +62,22 @@ Partial Class SelectServerDir
         Me.SelectDir.Description = "Choisissez le dossier dans lequel va être installé le serveur"
         Me.SelectDir.RootFolder = System.Environment.SpecialFolder.MyComputer
         '
+        'ServerDirLabel
+        '
+        Me.ServerDirLabel.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.ServerDirLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ServerDirLabel.Location = New System.Drawing.Point(12, 9)
+        Me.ServerDirLabel.Name = "ServerDirLabel"
+        Me.ServerDirLabel.Size = New System.Drawing.Size(681, 37)
+        Me.ServerDirLabel.TabIndex = 5
+        Me.ServerDirLabel.Text = "Dans quel dossier va être installé votre serveur ?"
+        '
         'SelectServerDir
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(705, 95)
+        Me.ClientSize = New System.Drawing.Size(685, 138)
+        Me.Controls.Add(Me.ServerDirLabel)
         Me.Controls.Add(Me.NextBtn)
         Me.Controls.Add(Me.Browse)
         Me.Controls.Add(Me.ServerPath)
@@ -78,4 +92,5 @@ Partial Class SelectServerDir
     Friend WithEvents Browse As Button
     Friend WithEvents NextBtn As Button
     Friend WithEvents SelectDir As FolderBrowserDialog
+    Friend WithEvents ServerDirLabel As Label
 End Class
--- SelectServerDir.vb
+++ SelectServerDir.vb
@@ -7,22 +7,27 @@
     Private Sub NextBtn_Click(sender As Object, e As EventArgs) Handles NextBtn.Click
         If System.IO.Directory.Exists(ServerPath.Text.Trim()) Then
             Informations.ServerDir = ServerPath.Text.Trim()
+            'Redirection vers la bonne fenêtre selon le type de Serveur choisi précédemment (Vanilla, Forge, Spigot, Sponge)
+            Select Case Informations.ServerType
+                Case ServerTypes.VANILLA
+                    Me.Hide()
+                    Vanilla.Show()
+
+                Case ServerTypes.SPIGOT
+
+                Case ServerTypes.FORGE
+
+                Case ServerTypes.SPONGE
+
+            End Select
         Else
             MsgBox("Le chemin d'accès spécifié n'existe pas !", vbOKOnly + vbCritical + 4096, "Erreur")
         End If
+    End Sub
 
-        'Redirection vers la bonne fenêtre selon le type de Serveur choisi précédemment (Vanilla, Forge, Spigot, Sponge)
-        Select Case Informations.ServerType
-            Case ServerTypes.VANILLA
-                Me.Hide()
-                Vanilla.Show()
-
-            Case ServerTypes.SPIGOT
-
-            Case ServerTypes.FORGE
-
-            Case ServerTypes.SPONGE
-
-        End Select
+    Private Sub ServerPath_KeyUp(sender As Object, e As KeyEventArgs) Handles ServerPath.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            NextBtn.PerformClick()
+        End If
     End Sub
 End Class
