2aa4c8204931323cfdd080fa235f1f23982e4988
TIEB62 -- Titouan Petit <petit.titouan1@gmail.com>
2020-04-17 17:51:24 +0200

    Added Vanilla Download Function to Downloader Form

--- Downloader.Designer.vb
+++ Downloader.Designer.vb
@@ -22,9 +22,47 @@ Partial Class Downloader
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.DownloadBar = New System.Windows.Forms.ProgressBar()
+        Me.InfoLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'DownloadBar
+        '
+        Me.DownloadBar.Location = New System.Drawing.Point(12, 12)
+        Me.DownloadBar.MarqueeAnimationSpeed = 1
+        Me.DownloadBar.Name = "DownloadBar"
+        Me.DownloadBar.Size = New System.Drawing.Size(1004, 48)
+        Me.DownloadBar.Step = 1
+        Me.DownloadBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.DownloadBar.TabIndex = 0
+        Me.DownloadBar.UseWaitCursor = True
+        '
+        'InfoLabel
+        '
+        Me.InfoLabel.AutoSize = True
+        Me.InfoLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.InfoLabel.Location = New System.Drawing.Point(12, 67)
+        Me.InfoLabel.Name = "InfoLabel"
+        Me.InfoLabel.Size = New System.Drawing.Size(142, 25)
+        Me.InfoLabel.TabIndex = 1
+        Me.InfoLabel.Text = "Initialisation . . ."
+        Me.InfoLabel.UseWaitCursor = True
+        '
+        'Downloader
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Downloader"
+        Me.ClientSize = New System.Drawing.Size(1028, 101)
+        Me.Controls.Add(Me.InfoLabel)
+        Me.Controls.Add(Me.DownloadBar)
+        Me.Name = "Downloader"
+        Me.Text = "Downloading . . ."
+        Me.UseWaitCursor = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents DownloadBar As ProgressBar
+    Friend WithEvents InfoLabel As Label
 End Class
--- Downloader.vb
+++ Downloader.vb
@@ -1,3 +1,55 @@
-﻿Public Class Downloader
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json.Linq
 
+Public Class Downloader
+    Public Async Sub DownloadVanillaServer(ByVal VersionName As String, ByVal Invoker As Form, ByVal NextForm As Form)
+        'On masque le Formulaire qui a appelé la Fonction et on affiche ce Formulaire
+        Invoker.Hide()
+        Me.Show()
+        InfoLabel.Text = "Searching " + VersionName + " ..."
+
+        'On récupère le lien de téléchargement du server.jar
+        Dim VersionURL As String = Vanilla.Versions(VersionName)
+        Dim DownloadPath As String = Informations.TempPath + "\" + VersionName + ".json"
+        If File.Exists(DownloadPath) Then
+            File.Delete(DownloadPath)
+        End If
+        My.Computer.Network.DownloadFile(VersionURL, DownloadPath)
+
+        Dim VersionJSON As String = File.ReadAllText(DownloadPath)
+
+        Dim Obj As JObject = JObject.Parse(VersionJSON)
+        Dim Downloads = Obj("downloads").ToList()
+        Dim DownloadLink As String
+        For Each Download As JProperty In Downloads
+            Download.CreateReader()
+            If Download.Name = "server" Then
+                Dim DownloadObj As JObject = JObject.Parse(Download.Value.ToString())
+                DownloadLink = DownloadObj("url").ToString()
+            End If
+        Next
+
+        'On télécharge le server.jar
+        InfoLabel.Text = "Downloading server.jar..."
+        DownloadBar.Style = ProgressBarStyle.Continuous
+        Dim Client As New WebClient
+        AddHandler Client.DownloadProgressChanged, AddressOf Client_ProgressChanged
+#Disable Warning BC42104 ' La variable est utilisée avant de se voir attribuer une valeur
+        If File.Exists(Informations.ServerDir + "\server.jar") Then
+            File.Delete(Informations.ServerDir + "\server.jar")
+        End If
+        Await Client.DownloadFileTaskAsync(New Uri(DownloadLink), Informations.ServerDir + "\server.jar")
+#Enable Warning BC42104 ' La variable est utilisée avant de se voir attribuer une valeur
+        InfoLabel.Text = "Téléchargement terminé !"
+
+        'On à fini le Téléchargement, on affiche le Formulaire suivant et on cache ce Formulaire
+        NextForm.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Client_ProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
+        DownloadBar.Value = e.ProgressPercentage
+    End Sub
 End Class
