41673dd950c38ca7511f59f2b204fb9386074d94
TIEB62 -- Titouan Petit <petit.titouan1@gmail.com>
2020-04-25 21:37:42 +0200

    Update Spigot Window

--- Spigot.Designer.vb
+++ Spigot.Designer.vb
@@ -22,9 +22,60 @@ Partial Class Spigot
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SpigotVersionsListBox = New System.Windows.Forms.ListBox()
+        Me.NextBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SpigotVersionsListBox
+        '
+        Me.SpigotVersionsListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.SpigotVersionsListBox.Font = New System.Drawing.Font("Comic Sans MS", 10.0!)
+        Me.SpigotVersionsListBox.FormattingEnabled = True
+        Me.SpigotVersionsListBox.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.SpigotVersionsListBox.ItemHeight = 18
+        Me.SpigotVersionsListBox.Items.AddRange(New Object() {"Dernière Version"})
+        Me.SpigotVersionsListBox.Location = New System.Drawing.Point(12, 46)
+        Me.SpigotVersionsListBox.Name = "SpigotVersionsListBox"
+        Me.SpigotVersionsListBox.Size = New System.Drawing.Size(215, 326)
+        Me.SpigotVersionsListBox.TabIndex = 1
+        '
+        'NextBtn
+        '
+        Me.NextBtn.Font = New System.Drawing.Font("Comic Sans MS", 20.0!)
+        Me.NextBtn.Location = New System.Drawing.Point(12, 386)
+        Me.NextBtn.Name = "NextBtn"
+        Me.NextBtn.Size = New System.Drawing.Size(215, 52)
+        Me.NextBtn.TabIndex = 2
+        Me.NextBtn.Text = "Suivant"
+        Me.NextBtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 10.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(215, 19)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Choisissez la version de Spigot :"
+        '
+        'Spigot
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(240, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NextBtn)
+        Me.Controls.Add(Me.SpigotVersionsListBox)
+        Me.Name = "Spigot"
         Me.Text = "Spigot"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents SpigotVersionsListBox As ListBox
+    Friend WithEvents NextBtn As Button
+    Friend WithEvents Label1 As Label
 End Class
--- Spigot.vb
+++ Spigot.vb
@@ -1,3 +1,20 @@
-﻿Public Class Spigot
+﻿Imports System.IO
 
+Public Class Spigot
+    Private Sub Spigot_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ConfirmClose(e)
+    End Sub
+
+    Private Sub Spigot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If CheckInternet() Then
+            DownloadFile(SpigotVersionsURL, SpigotVersionsDownloadPath)
+            Dim ListSpigotVersionsReader = File.ReadAllLines(SpigotVersionsDownloadPath)
+            For Each SpigotVersion As String In ListSpigotVersionsReader
+                SpigotVersionsListBox.Items.Add(SpigotVersion)
+            Next
+        Else
+                MsgBox("Aucune connection internet détectée !", vbCritical + vbOKOnly + 4096, "Erreur")
+            Application.Exit()
+        End If
+    End Sub
 End Class
