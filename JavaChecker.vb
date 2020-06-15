76718eb0ab59d0f8d947fd734e6f4a975200fbbd
TIEB62 -- Titouan Petit <petit.titouan1@gmail.com>
2020-06-15 15:30:35 +0200

    Create JavaChecker.vb
    Used for Java Installation Check

---
+++ JavaChecker.vb
@@ -0,0 +1,37 @@
+﻿Public Class JavaChecker
+
+    Public Enum JavaStatus
+        INSTALLED
+        NOT_INSTALLED
+        NOT_VERIFIED
+        VERIFYING
+        VERIFY_ERROR
+    End Enum
+
+    Public CurrentStatus As JavaStatus
+
+    Public Sub New()
+        CurrentStatus = JavaStatus.NOT_VERIFIED
+    End Sub
+
+    Public Sub SyncCheck()
+        CurrentStatus = JavaStatus.VERIFYING
+        Dim JavaChecker As Process = Process.Start("java -version")
+        Try
+            JavaChecker.WaitForExit(30000)
+            If JavaChecker.ExitCode <> 0 Then ' <> ==> "Différent de"
+                CurrentStatus = JavaStatus.NOT_INSTALLED
+            Else
+                CurrentStatus = JavaStatus.INSTALLED
+            End If
+        Catch ex As Exception
+            CurrentStatus = JavaStatus.VERIFY_ERROR
+        End Try
+    End Sub
+
+    Public Async Function AsyncCheck() As Task
+        Await Task.Run(New Action(AddressOf SyncCheck))
+    End Function
+
+
+End Class
